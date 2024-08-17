86d7f588b985664cbc0ce9be7fccec3942f954cb
Rishi Pradeep <67963002+RishiPradeep@users.noreply.github.com>
2024-08-17 23:08:22 +0530

    Update firebase.js

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -2,13 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0XhryXhlRMVWf3uAapa0X2BwvZUEGhgY",
-  authDomain: "story-demo-cbd02.firebaseapp.com",
-  projectId: "story-demo-cbd02",
-  storageBucket: "story-demo-cbd02.appspot.com",
-  messagingSenderId: "298696764133",
-  appId: "1:298696764133:web:13a8c98fad652e5af5820c",
-  measurementId: "G-H9V5N2SMQW",
+  // Your config here
 };
 
 const app = initializeApp(firebaseConfig);
